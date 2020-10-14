70cd8275c8a69bce67137fff2059a4b23478ef98
熊志刚 <xiongzhigang@pwrd.com>
2020-10-14 13:46:21 +0800

    feat(newImg): add new image of the article

--- test.py
+++ test.py
@@ -2,4 +2,4 @@
 # -*- coding: UTF-8 -*-
 import os
 
-os.popen('rm -rf web && git add . && git commit -m "feat(newImg): add new image of the article" && git push -u origin master')
+os.popen('rm -rf web && git add . && git commit -m "feat(newImg): add new image of the article" && git push')
